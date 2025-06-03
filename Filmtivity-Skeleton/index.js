5fb40ca6e72a56fc4454b9409b4fc2f9c0a6f7ce
Ivan Ivanov <ivan.ivanov.highschool@buditel.bg>
2025-06-03 10:39:40 +0300

    Add initial project setup with package.json and seed script for MongoDB
    - Created package.json with dependencies and scripts for the project.
    - Implemented seed.js to populate the database with test users and movies.
    - Established connection to MongoDB and included error handling for seeding process.

---
+++ Filmtivity-Skeleton/index.js
@@ -0,0 +1,53 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
+import {fileURLToPath} from 'url';
+import {dirname, join} from 'path';
+import {engine} from 'express-handlebars';
+
+dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => console.log('MongoDB connected'))
+.catch((err) => console.log(err));
+
+app.engine(".hbs", engine({
+    extname: ".hbs",
+    //defaultLayout: "main",
+    layoutsDir: join(__dirname, "views/layouts"),
+    partialsDir: join(__dirname, "views/partials"),
+}));
+app.set("view engine", "hbs");
+app.set("views", join(__dirname, "views"));
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
+app.use(express.static(join(__dirname, "static")));
+
+app.use('/src', express.static(join(__dirname, 'src')));
+
+app.get("/", (req, res) => {
+    res.render('pages/home', {
+        title: 'Home',
+        additionalStyles: ['home']
+    });
+});
+
+app.use((req, res) => {
+    res.status(404).render('pages/404', {
+        title: 'Page Not Found',
+        additionalStyles: ['error']
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
